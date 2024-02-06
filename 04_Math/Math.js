2a96fb0d0f52aa6f633e4e4ebea2c239f03748be
mdali017 <mohabbatalit8@gmail.com>
2024-02-07 01:09:29 +0600

    math functionality added in website

---
+++ 04_Math/Math.js
@@ -0,0 +1,36 @@
+// console.log("Math is Comming Soon....");
+
+// let num = 30;
+// let num = 30.999992;
+// let num = 30.475;
+let num = 30.000000000001;
+// console.log(num);
+// console.log(Math.round(num)); // 30 (1-4)
+// console.log(Math.round(num)); // 31      (5-9)
+// console.log(Math.ceil(num)); // 30
+// console.log(Math.floor(num)); // 30
+
+let num1 = 30.999345;
+let num2 = 2;
+
+// square
+// console.log(Math.pow(4, 3)); // 64
+
+// Root Value
+// console.log(Math.sqrt(64)); // 8
+
+// absolute number
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+
+// min and max
+// console.log(Math.max(2, 4, 6, 5, 3, 9));
+// console.log(Math.min(2, 4, 6, 5, 3, 9));
+
+// PI
+// console.log(Math.PI);
+
+// random number
+// console.log(Math.random());
+
+console.log(Math.random().toFixed(2) * 10);
